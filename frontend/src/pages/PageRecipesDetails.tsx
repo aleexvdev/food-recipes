cc04cdf547a7611bc5efe82cf57adb34ce5c0ee2
aleexvdev <valverdealex25@gmail.com>
2024-03-25 00:16:23 -0500

    fix (responsive table page recipe details)

--- frontend/src/pages/PageRecipesDetails.tsx
+++ frontend/src/pages/PageRecipesDetails.tsx
@@ -41,8 +41,8 @@ export const PageRecipesDetails = () => {
   return (
     <main className='h-auto'>
       <section className='h-auto min-h-screen w-full mx-auto max-w-6xl transition-opacity'>
-        <div className='w-full h-full flex flex-col-reverse md:flex-row items-start justify-start pt-28 md:pt-32 pb-20 px-5 md:px-0'>
-          <div className='w-full md:w-[30%]'>
+        <div className='w-full h-full flex flex-col-reverse lg:flex-row items-start justify-start pt-28 md:pt-32 pb-20 px-5 lg:px-0'>
+          <div className='w-full lg:w-[30%]'>
             <img 
               src={recipe?.recipe?.images.regular?.url} 
               alt={recipe?.recipe?.label}  
@@ -50,39 +50,39 @@ export const PageRecipesDetails = () => {
               className='w-full h-96 object-cover rounded-lg'
             />
           </div>
-          <div className='w-full md:w-[70%] h-full pl-5'>
+          <div className='w-full lg:w-[70%] h-full pl-5'>
             <AnimatePresence>
-              <div className='flex flex-col-reverse md:flex-row items-start justify-between gap-5 w-full'>
+              <div className='flex flex-col-reverse md:flex-row items-start justify-between gap-5 md:gap-0 w-full'>
                 <h1 className='text-3xl md:text-5xl banner-title font-bold'>{recipe?.recipe?.label}</h1>
                 <div className='w-full flex items-center justify-end gap-3'>
-                  <button className="flex items-center justify-center gap-1 rounded-lg bg-first hover:bg-first/90 text-white py-1 px-3">
+                  <button className="flex items-center justify-center gap-1 rounded-lg bg-first hover:bg-first/90 text-white py-1 px-4">
                     <MdOutlineBookmarkAdd className="w-6 h-6" />
-                    <span>Save</span>
+                    <span className='text-xl md:text-lg'>Save</span>
                   </button>
-                  <button className='flex items-center justify-center gap-1 rounded-lg bg-first hover:bg-first/90 text-white py-1 px-3'>
+                  <button className='flex items-center justify-center gap-1 rounded-lg bg-first hover:bg-first/90 text-white py-1 px-4'>
                     <LuPrinter className='w-6 h-6 text-white' />
-                    <span>Print</span>
+                    <span className='text-xl md:text-lg'>Print</span>
                   </button>
                 </div>
               </div>
               <div className='pt-3 w-full'>
                 <Link to={recipe?.recipe?.url ? recipe.recipe.url : '/'} target='_blank'>
-                  <span className='w-full'>by <span className='w-full font-medium text-lg'>{recipe?.recipe?.source}</span></span>
+                  <span className='w-full md:text-xl'>by <span className='w-full font-medium text-lg md:text-2xl'>{recipe?.recipe?.source}</span></span>
                 </Link>
               </div>
               <div className='py-5 w-full'>
                 <div className='grid grid-cols-3'>
                   <div className='flex flex-col items-center justify-center'>
                     <span className='font-bold text-4xl md:text-7xl banner-title'>{recipe?.recipe?.ingredientLines.length}</span>
-                    <span className='font-medium'>Ingredients</span>
+                    <span className='font-medium text-lg md:text-2xl'>Ingredients</span>
                   </div>
                   <div className='flex flex-col items-center justify-center border-x-2 border-gray-300'>
                     <span className='font-bold text-4xl md:text-7xl banner-title'>{recipe?.recipe?.totalTime}</span>
-                    <span className='font-medium'>Minutes</span>
+                    <span className='font-medium text-lg md:text-2xl'>Minutes</span>
                   </div>
                   <div className='flex flex-col items-center justify-center'>
                     <span className='font-bold text-4xl md:text-7xl banner-title'>{recipe?.recipe?.calories.toFixed(0)}</span>
-                    <span className='font-medium'>Calories</span>
+                    <span className='font-medium text-lg md:text-2xl'>Calories</span>
                   </div>
                 </div>
               </div>
@@ -105,9 +105,9 @@ export const PageRecipesDetails = () => {
               <div className='w-full pt-6 flex items-center justify-between border-b-2 border-gray-300 pb-3'>
                 <div className='flex items-center gap-2'>
                   <PiForkKnifeFill className='w-6 h-6 text-black'/>
-                  <span className='font-bold text-2xl md:text-3xl banner-title'>Ingredients</span>
+                  <span className='font-bold text-2xl md:text-4xl banner-title'>Ingredients</span>
                 </div>
-                <span className='text-sm'>for {recipe?.recipe?.yield.toFixed(0)} servings</span>
+                <span className='text-sm md:text-lg'>for {recipe?.recipe?.yield.toFixed(0)} servings</span>
               </div>
               <div className='w-full pt-5 md:pt-3 flex flex-col items-center justify-center pb-8'>
                 {
@@ -125,7 +125,7 @@ export const PageRecipesDetails = () => {
                           <span className='w-10 h-full flex items-center justify-center'>
                             <IoCheckmarkOutline className='w-6 h-6 text-first' />
                           </span>
-                          <span className='w-full text-md font-normal text-black'>
+                          <span className='w-full text-lg md:text-xl font-normal text-black'>
                             {ingredient}
                           </span>
                         </div>
