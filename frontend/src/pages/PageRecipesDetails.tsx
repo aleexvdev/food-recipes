76f2ee9ffd33869c86e9143cc011b9efa2a3099b
aleexvdev <valverdealex25@gmail.com>
2024-03-25 00:04:21 -0500

    feat(responsive recipe detail)

--- frontend/src/pages/PageRecipesDetails.tsx
+++ frontend/src/pages/PageRecipesDetails.tsx
@@ -41,8 +41,8 @@ export const PageRecipesDetails = () => {
   return (
     <main className='h-auto'>
       <section className='h-auto min-h-screen w-full mx-auto max-w-6xl transition-opacity'>
-        <div className='w-full h-full flex items-start justify-start pt-32 pb-20'>
-          <div className='w-[30%]'>
+        <div className='w-full h-full flex flex-col-reverse md:flex-row items-start justify-start pt-28 md:pt-32 pb-20 px-5 md:px-0'>
+          <div className='w-full md:w-[30%]'>
             <img 
               src={recipe?.recipe?.images.regular?.url} 
               alt={recipe?.recipe?.label}  
@@ -50,11 +50,11 @@ export const PageRecipesDetails = () => {
               className='w-full h-96 object-cover rounded-lg'
             />
           </div>
-          <div className='w-[70%] h-full pl-5'>
+          <div className='w-full md:w-[70%] h-full pl-5'>
             <AnimatePresence>
-              <div className='flex items-start justify-between'>
-                <h1 className='text-5xl banner-title font-bold'>{recipe?.recipe?.label}</h1>
-                <div className='flex items-center justify-end gap-3'>
+              <div className='flex flex-col-reverse md:flex-row items-start justify-between gap-5 w-full'>
+                <h1 className='text-3xl md:text-5xl banner-title font-bold'>{recipe?.recipe?.label}</h1>
+                <div className='w-full flex items-center justify-end gap-3'>
                   <button className="flex items-center justify-center gap-1 rounded-lg bg-first hover:bg-first/90 text-white py-1 px-3">
                     <MdOutlineBookmarkAdd className="w-6 h-6" />
                     <span>Save</span>
@@ -73,43 +73,43 @@ export const PageRecipesDetails = () => {
               <div className='py-5 w-full'>
                 <div className='grid grid-cols-3'>
                   <div className='flex flex-col items-center justify-center'>
-                    <span className='font-bold text-7xl banner-title'>{recipe?.recipe?.ingredientLines.length}</span>
+                    <span className='font-bold text-4xl md:text-7xl banner-title'>{recipe?.recipe?.ingredientLines.length}</span>
                     <span className='font-medium'>Ingredients</span>
                   </div>
                   <div className='flex flex-col items-center justify-center border-x-2 border-gray-300'>
-                    <span className='font-bold text-7xl banner-title'>{recipe?.recipe?.totalTime}</span>
+                    <span className='font-bold text-4xl md:text-7xl banner-title'>{recipe?.recipe?.totalTime}</span>
                     <span className='font-medium'>Minutes</span>
                   </div>
                   <div className='flex flex-col items-center justify-center'>
-                    <span className='font-bold text-7xl banner-title'>{recipe?.recipe?.calories.toFixed(0)}</span>
+                    <span className='font-bold text-4xl md:text-7xl banner-title'>{recipe?.recipe?.calories.toFixed(0)}</span>
                     <span className='font-medium'>Calories</span>
                   </div>
                 </div>
               </div>
               <div className='pt-4 w-full'>
-                <div className='w-full flex items-center justify-start gap-3'>
-                  <button className='border-2 px-2 rounded-md border-first bg-first/50 hover:bg-first/40 flex items-center justify-center gap-2 h-8'>
+                <div className='w-full flex items-center justify-start gap-3 flex-wrap'>
+                  <button className='border-2 px-2 rounded-md border-first bg-first/50 hover:bg-first/40 flex items-center justify-center gap-2 w-auto h-8'>
                     <TfiWorld className='w-5 h-4' />
                     <span className='text-sm font-medium text-black'>{recipe?.recipe?.cuisineType[0]}</span>
                   </button>
-                  <button className='border-2 px-2 rounded-md border-first bg-first/50 hover:bg-first/40 flex items-center justify-center gap-2 h-8'>
-                    <BiDish className='w-5 h-5' />
-                    <span className='text-sm font-medium text-black'>{recipe?.recipe?.dishType[0]}</span>
-                  </button>
-                  <button className='border-2 px-2 rounded-md border-first bg-first/50 hover:bg-first/40 flex items-center justify-center gap-2 h-8'>
+                  <button className='border-2 px-2 rounded-md border-first bg-first/50 hover:bg-first/40 flex items-center justify-center gap-2 w-auto h-8'>
                     <PiBowlFood className='w-5 h-5' />
                     <span className='text-sm font-medium text-black'>{recipe?.recipe?.mealType[0]}</span>
+                  </button>
+                  <button className='border-2 px-2 rounded-md border-first bg-first/50 hover:bg-first/40 flex items-center justify-center gap-2 w-auto h-8'>
+                    <BiDish className='w-5 h-5' />
+                    <span className='text-sm font-medium text-black'>{recipe?.recipe?.dishType[0]}</span>
                   </button>
                 </div>
               </div>
               <div className='w-full pt-6 flex items-center justify-between border-b-2 border-gray-300 pb-3'>
                 <div className='flex items-center gap-2'>
                   <PiForkKnifeFill className='w-6 h-6 text-black'/>
-                  <span className='font-bold text-xl banner-title'>Ingredients</span>
+                  <span className='font-bold text-2xl md:text-3xl banner-title'>Ingredients</span>
                 </div>
                 <span className='text-sm'>for {recipe?.recipe?.yield.toFixed(0)} servings</span>
               </div>
-              <div className='w-full pt-3 flex flex-col items-center justify-center'>
+              <div className='w-full pt-5 md:pt-3 flex flex-col items-center justify-center pb-8'>
                 {
                   recipe?.recipe?.ingredientLines.map((ingredient, index) => {
                     return (
